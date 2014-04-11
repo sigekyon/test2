d0fdb2be88dfb378cd019cc3e38b6a84a944e0e0
thorie <thorie@ntc.co.jp>
2014-04-11 15:40:38 +0900

    1

---
+++ 0411/mondai3e.java
@@ -0,0 +1,31 @@
+package rensyu;
+
+public class mondai3e {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("1:検索　2:登録　3:削除　4:変更");
+		
+		int selected = new java.util.Scanner(System.in).nextInt();
+		
+		switch(selected){
+		
+			case 1:
+				System.out.println("検索します。");
+				break;
+			case 2:
+				System.out.println("登録します。");
+				break;
+			case 3:
+				System.out.println("削除します。");
+				break;
+			case 4:
+				System.out.println("変更します。");
+				break;
+			default:
+				
+		}
+
+	}
+
+}
